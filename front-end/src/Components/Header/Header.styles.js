3d4c0e6434dcc38d24308a81cb4c77c48a7ce2fe
ali varaste pour <alivaraste@outlook.com>
2021-12-13 09:41:03 -0700

    MainPage Header styles are added

---
+++ front-end/src/Components/Header/Header.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    height: 70px;
+    display: flex;
+    justify-content: center;
+
+    .btn{
+        border-radius: 10px 10px 0 0;
+        outline: none;
+        border: none;
+        width: 120px;
+        height: 70px;
+        font-family: 'Dinar','Tanha', sans-serif;
+        font-size: 1.5rem;
+        margin-left: 10px;
+        background-color: #F5F5F5;
+    }
+`;
