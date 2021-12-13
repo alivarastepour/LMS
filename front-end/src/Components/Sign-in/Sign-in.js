c0074d05880395c275b38c31128a41fbccafaa2c
ali varaste pour <alivaraste@outlook.com>
2021-12-13 10:03:58 -0700

    sign-in content is added

--- front-end/src/Components/Sign-in/Sign-in.js
+++ front-end/src/Components/Sign-in/Sign-in.js
@@ -1,9 +1,21 @@
 import { Wrapper, Content } from "./sign-in.styles";
-
+import './sign-in.css'
 const SignIn = () => {
     return <>
         <Wrapper>
-            <Content/>
+            <Content>
+                <div className="flex-item label">نام کاربری</div>
+                <div className="flex-item input">
+                    <input type='text' placeholder="نام کاربری"/>
+                </div>
+                <div className="flex-item label" >رمز عبور</div>
+                <div className="flex-item input">
+                    <input type='password' placeholder="رمز عبور"/>
+                </div>
+                <div className="flex-item submit">
+                    <button>login</button>
+                </div>
+            </Content>
         </Wrapper>
     </>
 }
