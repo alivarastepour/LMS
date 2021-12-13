2e33baae44db34472b9d6422323983c07a559f60
ali varaste pour <alivaraste@outlook.com>
2021-12-13 09:41:56 -0700

    Sign-in initial styles are added

---
+++ front-end/src/Components/Sign-in/sign-in.styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 600px;
+    height: 600px;
+    margin: auto;
+    background-color: #F5F5F5;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 10px;
+    padding-top: 50px;
+`;
+
+export const Content = styled.div`
+    max-width: 400px;
+    height: 500px;
+    margin: auto;
+    border: 1px black solid;
+`;
