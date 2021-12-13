2a58b4a060ed368378d624cb535ef04c8284e569
Mahmood Choopani <choopani.m81@gmail.com>
2021-12-13 09:01:36 +0330

    base structure of backend initialized.

---
+++ back-end/lms/StudyArena/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class School(models.Model):
+    pass
+
+
+class Class(models.Model):
+    pass
+
+
+class Teacher(models.Model):
+    pass
+
+
+class Manager(models.Model):
+    pass
+
+
+class Student(models.Model):
+    pass
