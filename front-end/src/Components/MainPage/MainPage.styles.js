6f0bb1149b534736172c604a5dd7d4292f3c5653
ali varaste pour <alivaraste@outlook.com>
2021-12-13 01:20:53 -0700

    MainPage styles are improved

--- front-end/src/Components/MainPage/MainPage.styles.js
+++ front-end/src/Components/MainPage/MainPage.styles.js
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
     width: 1000px;
     height: 700px;
-    border: 1px solid black;
-    margin: 100px auto;
+    margin: auto auto;
+    backdrop-filter: blur(30px) saturate(160%);
+    -webkit-backdrop-filter: blur(30px) saturate(160%);
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 20px;
+
+    .body{
+        height: 630px;
+    }
 `;
