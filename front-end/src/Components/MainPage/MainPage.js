c1ebb4dd6744ad194c09751b4398e6648ed2b3dd
ali varaste pour <alivaraste@outlook.com>
2021-12-13 09:42:35 -0700

    main components are rendered

--- front-end/src/Components/MainPage/MainPage.js
+++ front-end/src/Components/MainPage/MainPage.js
@@ -1,8 +1,15 @@
 import { Wrapper } from "./MainPage.styles";
+import Header from "../Header/Header";
+import SignIn from "../Sign-in/Sign-in";
+import '../../Assets/styles/general-styles.css';
+import React from "react";
 
 const MainPage = () => {
     return <>
-        <Wrapper/>
+        <Wrapper>
+            <Header/>
+            <SignIn/>
+        </Wrapper>
     </>
 }
 
