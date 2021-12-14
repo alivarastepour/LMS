7adb4185c25c4693c64f76412b4de4f0cfeaaf54
Ali <alivaraste@outlook.com>
2021-12-15 01:25:23 +0330

    useReducer is initialized

--- front-end/src/Components/Sign-up/Sign-up.js
+++ front-end/src/Components/Sign-up/Sign-up.js
@@ -1,32 +1,66 @@
 import {Wrapper} from "../Sign-in/sign-in.styles";
 import {Content} from "./Sign-up.styles";
-import {useState} from "react";
-import {signUpHandler, setUser, setPass, set_Email  } from "./Sign-up.handlers";
+import {useReducer} from "react";
+import {signUpHandler, valid_username, valid_password, valid_email, valid_role} from "./Sign-up.handlers";
+import {signUpReducer} from "./Sign-up.reducer";
 
 const SignUp = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [role, setRole] = useState('');
+
+    const initialState = {
+        username:'',
+        password:'',
+        email:'',
+        role:'manager',
+        validUsername:true,
+        validPassword:true,
+        validEmail:true,
+    }
+
+    const [state, dispatch] = useReducer(signUpReducer,initialState);
+    const {username, password, email, role, validUsername, validPassword, validEmail} = state ;
+
     return <>
         <Wrapper>
             <Content>
-                <form onSubmit={(e) => signUpHandler(e, username, password, email, role)}>
+                <form
+                    onSubmit={(e) => signUpHandler(e, state)}>
                     <div className="flex-item labelX">نام کاربری</div>
                     <div className="flex-item inputX">
-                        <input autoFocus value={username} onChange={e => setUser(e.target.value, setUsername)} className="inp" type='text' placeholder="کد ملی"/>
+                        <input
+                            autoFocus
+                            value={username}
+                            onChange={e => dispatch({type:'SET-USERNAME', payload:e.target.value})}
+                            onBlur={() => valid_username(username, dispatch)}
+                            className={`${validUsername ?  'inp' : 'inp error'}`}
+                            type='text'
+                            placeholder="کد ملی"/>
                     </div>
                     <div className="flex-item labelX" >رمز عبور</div>
                     <div className="flex-item inputX">
-                        <input value={password} onChange={e => setPass(e.target.value, setPassword)} className="inp" type='text' placeholder="رمز عبور"/>
+                        <input
+                            value={password}
+                            onChange={e => dispatch({type:'SET-PASSWORD', payload:e.target.value})}
+                            onBlur={() => valid_password(password, dispatch)}
+                            className={`${validPassword ? 'inp' : 'inp error'}`}
+                            type='text'
+                            placeholder="رمز عبور"/>
                     </div>
                     <div className="flex-item labelX" >ایمیل</div>
                     <div className="flex-item inputX">
-                        <input value={email} onChange={e => set_Email(e.target.value, setEmail)} className="inp" type='text' placeholder="ایمیل"/>
+                        <input
+                            value={email}
+                            onChange={e => dispatch({type:'SET-EMAIL', payload:e.target.value})}
+                            onBlur={() => valid_email(email, dispatch)}
+                            className={`${validEmail ? 'inp' : 'inp error'}`}
+                            type='text'
+                            placeholder="ایمیل"/>
                     </div>
                     <div className="flex-item labelX" >نقش</div>
                     <div className="flex-item inputX">
-                        <select onChange={e => setRole(e.target.value)} className="select">
+                        <select defaultValue='manager'
+                            onChange={e => dispatch({type:'SET-ROLE', payload:e.target.value})}
+                            className='select'
+                        >
                             <option value='manager'>مدیر</option>
                             <option value='teacher'>دبیر/استاد</option>
                             <option value='student'>دانش‌آموز/دانشجو</option>
