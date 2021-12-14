685b50295d4993f26f07c77ed550b470566761ce
ali varaste pour <alivaraste@outlook.com>
2021-12-14 08:19:03 -0700

    role field is added

--- front-end/src/Components/Sign-up/Sign-up.js
+++ front-end/src/Components/Sign-up/Sign-up.js
@@ -7,13 +7,14 @@ const SignUp = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
+    const [role, setRole] = useState('');
     return <>
         <Wrapper>
             <Content>
-                <form onSubmit={(e) => signUpHandler(e, username, password, email)}>
+                <form onSubmit={(e) => signUpHandler(e, username, password, email, role)}>
                     <div className="flex-item labelX">نام کاربری</div>
                     <div className="flex-item inputX">
-                        <input value={username} onChange={e => setUser(e.target.value, setUsername)} className="inp" type='text' placeholder="کد ملی"/>
+                        <input autoFocus value={username} onChange={e => setUser(e.target.value, setUsername)} className="inp" type='text' placeholder="کد ملی"/>
                     </div>
                     <div className="flex-item labelX" >رمز عبور</div>
                     <div className="flex-item inputX">
@@ -22,6 +23,14 @@ const SignUp = () => {
                     <div className="flex-item labelX" >ایمیل</div>
                     <div className="flex-item inputX">
                         <input value={email} onChange={e => set_Email(e.target.value, setEmail)} className="inp" type='text' placeholder="ایمیل"/>
+                    </div>
+                    <div className="flex-item labelX" >نقش</div>
+                    <div className="flex-item inputX">
+                        <select onChange={e => setRole(e.target.value)} className="select">
+                            <option value='manager'>مدیر</option>
+                            <option value='teacher'>دبیر/استاد</option>
+                            <option value='student'>دانش‌آموز/دانشجو</option>
+                        </select>
                     </div>
                     <div className="flex-item submitX">
                         <button type='submit' className="buttonX">ثبت نام</button>
