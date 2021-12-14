dd568974b07403d22b2f8c81c87f7f557f17ae72
Mahmood Choopani <choopani.m81@gmail.com>
2021-12-14 08:55:48 +0330

    url generator wrote.

--- back-end/lms/StudyArena/BBBApiConnection.py
+++ back-end/lms/StudyArena/BBBApiConnection.py
@@ -1,12 +1,25 @@
 import hashlib
+import requests
 
-SHARED_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXX'
+SHARED_SECRET = 'XXXXXXXXXXXXXXXXX'
+SERVER_ADDRESS = 'X.Y.com/bigbluebutton/api/'
 
 
-def create_checksum(method: str, **kwargs) -> str:
+def dict_to_str(**kwargs):
     query = ''
     for param_name, param_value in kwargs.items():
         query += f"{param_name}={param_value}&"
-    query = method + query[:-1] + SHARED_SECRET
+    return query[:-1]
+
+
+def create_checksum(method: str, **kwargs) -> str:
+    query = method + dict_to_str(**kwargs) + SHARED_SECRET
     return hashlib.sha1(query.encode()).hexdigest()
 
+
+def generate_url(method, **kwargs):
+    return SERVER_ADDRESS + method + '?' + dict_to_str(**kwargs) + '&checksum=' + create_checksum(method, **kwargs)
+
+
+if __name__ == '__main__':
+    print(generate_url('create', name='Test+Meeting', meetingID='abc123', attendeePW='111222', moderatorPW='333444'))
