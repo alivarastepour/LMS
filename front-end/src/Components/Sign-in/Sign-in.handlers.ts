6f62fcc9ed92b9a2b2b2114b08c9d79439ee353e
Ali <alivaraste@outlook.com>
2021-12-15 14:06:42 +0330

    data validation before sending request

--- front-end/src/Components/Sign-in/Sign-in.handlers.ts
+++ front-end/src/Components/Sign-in/Sign-in.handlers.ts
@@ -1,12 +1,33 @@
 import axios from "axios";
 import {FormEvent} from "react";
 
-export const submitHandler = (e:FormEvent<HTMLFormElement>, username:string, password:string):void => {
-    axios.post('http://localhost:8000/auth/login/', {
-        username:username,
-        password:password
-    }).then((res) => {
-        console.log(res.status);
-    }).catch(e => console.log(e));
-    e.preventDefault();
+export const submitHandler = (e:FormEvent<HTMLFormElement>, state: {username:string, password:string, validUsername:string, validPassword:string}, dispatch:Function):void => {
+    let vu, vp ;
+    if (state.username.length !== 10){
+        dispatch({type:'VALID-USERNAME', payload:false})
+        vu = false;
+    }
+    else{
+        dispatch({type:'VALID-USERNAME', payload:true})
+        vu = true;
+    }
+    if (state.password.trim() === ''){
+        dispatch({type:'VALID-PASSWORD', payload:false})
+        vp = false;
+    }
+    else{
+        dispatch({type:'VALID-PASSWORD', payload:true})
+        vp = true;
+    }
+    if (vu && vp){
+        axios.post('http://localhost:8000/auth/login/', {
+            username:state.username,
+            password:state.password
+        }).then((res) => {
+            console.log(res.data.token);
+        }).catch(e => console.log(e));
+    }else{
+        e.preventDefault();
+    }
+
 }
