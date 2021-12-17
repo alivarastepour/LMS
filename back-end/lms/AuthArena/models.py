a67dd5d0fa3a40632d7abccea5d1d05ad6602b4f
Mahmood Choopani <choopani.m81@gmail.com>
2021-12-17 13:55:05 +0330

    add role field to user and handle it from request.

--- back-end/lms/AuthArena/models.py
+++ back-end/lms/AuthArena/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
-    pass
+    role = models.CharField(choices=[('A', 'Admin'),
+                                     ('M', 'Manager'),
+                                     ('T', 'Teacher'),
+                                     ('S', 'Student')],
+                            max_length=1)
--- back-end/lms/AuthArena/serializers.py
+++ back-end/lms/AuthArena/serializers.py
@@ -5,11 +5,12 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'password', 'email')
+        fields = ('id', 'username', 'password', 'role', 'email')
 
     def create(self, validated_data):
         user = CustomUser.objects.create(username=validated_data['username'],
-                                         email=validated_data['email'])
+                                         email=validated_data['email'],
+                                         role=validated_data['role'])
         user.set_password(validated_data['password'])
         user.save()
         return user
