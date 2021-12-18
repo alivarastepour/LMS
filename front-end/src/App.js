6f9e331bf18b042457f97b04596ec01db4d07f14
ali varaste pour <alivaraste@outlook.com>
2021-12-18 11:46:30 -0700

    AuthContext is provided

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,23 @@
 import MainPage from "./Components/MainPage/MainPage";
 import '../src/Assets/styles/general-styles.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useReducer } from "react";
-import { reducer } from "./App.reducer";
+import { createContext, useState } from "react";
 
+export const authContext = createContext();
 
 const App = () => {
-  const initialState = {
-    loginState:false,
-    signupState:false
-  }
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-  console.log(state);
+  const [auth, setAuth] = useState(false);
 
   return (
     <Router>
+    <authContext.Provider value={{auth, setAuth}}>
       <Routes>
-        <Route path='/' element={<div className="App"><MainPage setProcessState={dispatch}/></div>}/>
+        <Route path='/' element={<div className="App"><MainPage/></div>}/>
+        <Route path='/accounts/manager/:id' element={<div>helloooo</div>}/>
       </Routes>
+    </authContext.Provider>
     </Router>
+
   );
 }
 
