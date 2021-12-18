afaeb941b4edb7562e34edfd2d7522df3792c495
ali varaste pour <alivaraste@outlook.com>
2021-12-17 22:22:37 -0700

    MainPage is redesigned

--- front-end/src/Components/MainPage/MainPage.js
+++ front-end/src/Components/MainPage/MainPage.js
@@ -1,18 +1,23 @@
-import { Wrapper } from "./MainPage.styles";
+import { Wrapper, Content, Heading } from "./MainPage.styles";
 import Header from "../Header/Header";
 import SignIn from "../Sign-in/Sign-in";
 import SignUp from "../Sign-up/Sign-up";
 import '../../Assets/styles/general-styles.css';
-import React, {useState} from "react";
+import {useState} from "react";
 
 const MainPage = () => {
     const [state, setState] = useState(true);
     return <>
         <Wrapper>
-            <Header login={state} setLogin={setState}/>
-            {
-                state ? <SignIn/> : <SignUp/>
-            }
+            <Content>
+                <Header login={state} setLogin={setState}/>
+                    {
+                        state ? <SignIn/> : <SignUp/>
+                    }
+            </Content>
+            <Heading>
+                <h1>سیستم آموزش آنلاین آتی‌گستر</h1>
+            </Heading>
         </Wrapper>
     </>
 }
