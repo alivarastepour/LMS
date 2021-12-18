20672459c50c013d9cb05170b0ec1504d4ee169c
ali varaste pour <alivaraste@outlook.com>
2021-12-17 22:23:45 -0700

    minor bug fix

--- front-end/src/Components/Sign-in/sign-in.styles.js
+++ front-end/src/Components/Sign-in/sign-in.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    max-width: 600px;
+    width: 600px;
     height: 600px;
     margin: auto;
     background-color: #F5F5F5;
