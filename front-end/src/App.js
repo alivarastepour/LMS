6a31da1717457a721d76b21550f88a4733b198d9
ali varaste pour <alivaraste@outlook.com>
2021-12-18 07:55:37 -0700

    Router is initialized

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,25 @@
 import MainPage from "./Components/MainPage/MainPage";
 import '../src/Assets/styles/general-styles.css'
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useReducer } from "react";
+import { reducer } from "./App.reducer";
+
+
+const App = () => {
+  const initialState = {
+    loginState:false,
+    signupState:false
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+
   return (
-    <div className="App">
-      <MainPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<div className="App"><MainPage setProcessState={dispatch}/></div>}/>
+      </Routes>
+    </Router>
   );
 }
 
