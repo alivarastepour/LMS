1a84f2dd40f3e832919a0e2f625c259f664e4e71
ali varaste pour <alivaraste@outlook.com>
2021-12-17 22:23:34 -0700

    styles are redesigned

--- front-end/src/Components/MainPage/MainPage.styles.js
+++ front-end/src/Components/MainPage/MainPage.styles.js
@@ -1,15 +1,29 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-    max-width: 1000px;
-    height: 700px;
-    margin: auto auto;
-    backdrop-filter: blur(30px) saturate(160%);
+    max-width: 95%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap-reverse;
+    padding-left: 30px;
+    padding-bottom: 173px;
+`;
+
+export const Content = styled.div`
+    backdrop-filter: blur(31px) saturate(160%);
     -webkit-backdrop-filter: blur(30px) saturate(160%);
     background-color: rgba(255, 255, 255, 0.75);
-    border-radius: 20px;
+    border-radius: 20px; 
+    height: 700px;
+    padding: 0 30px;
+`;
 
-    .body{
-        height: 630px;
-    }
+export const Heading = styled.div`
+    height: 700px;
+    padding-top: 100px;
+    font-size: 2.0rem;
+    color: white;
+    text-shadow: 0 1px 0 black;
+    text-align: center;
 `;
