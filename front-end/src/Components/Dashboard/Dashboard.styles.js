70eb6a35ab362c13dba294cc00eef38fdae94702
ali varaste pour <alivaraste@outlook.com>
2021-12-19 09:50:39 -0700

    Dashboard initial styles are added

---
+++ front-end/src/Components/Dashboard/Dashboard.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    width: 95%;
+    height: 1000px;
+    margin: auto auto 100px auto;
+    border: 1px solid black;
+`;
