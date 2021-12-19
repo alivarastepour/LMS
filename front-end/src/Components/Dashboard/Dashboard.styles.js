2dc3b099967de551ca071c28249a8feda887bd70
ali varaste pour <alivaraste@outlook.com>
2021-12-19 10:51:39 -0700

    more styles for Dashboard

--- front-end/src/Components/Dashboard/Dashboard.styles.js
+++ front-end/src/Components/Dashboard/Dashboard.styles.js
@@ -4,5 +4,8 @@ export const Wrapper = styled.div`
     width: 95%;
     height: 1000px;
     margin: auto auto 100px auto;
-    border: 1px solid black;
-`;
+    backdrop-filter: blur(31px) saturate(160%);
+    -webkit-backdrop-filter: blur(30px) saturate(160%);
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 10px; 
+    `;
