96375c5816329acd01964c7f49e92b56b2c9cb7f
ali varaste pour <alivaraste@outlook.com>
2021-12-19 23:39:50 -0700

    Profile styles are added

---
+++ front-end/src/Components/Profile/Profile.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    width: 90%;
+    height: 90%;
+    margin: auto;
+    background-color: #F5F5F5;
+    border-radius: 10px;
+`;
