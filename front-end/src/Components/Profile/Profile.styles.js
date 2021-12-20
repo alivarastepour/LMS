f5f59ef13d672dbe74dbb6946903b7d93df89a43
ali varaste pour <alivaraste@outlook.com>
2021-12-20 09:52:02 -0700

    Profile component styles

--- front-end/src/Components/Profile/Profile.styles.js
+++ front-end/src/Components/Profile/Profile.styles.js
@@ -6,4 +6,47 @@ export const Wrapper = styled.div`
     margin: auto;
     background-color: #F5F5F5;
     border-radius: 10px;
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-template-rows: 20% 20% 20%;
+    justify-content: space-evenly;
+
+    .label{
+        font-family: 'vazir', sans-serif;
+        font-size: 1.4rem;
+    }
+    .content{
+        font-size: 1.2rem;
+    }
+
+    .grid-item{
+        margin: auto;
+    }
+    .profile-image{
+        border-radius: 50%;
+        width: 299px;
+        height: 299px;
+    }
+    .item1{
+        grid-row: 1/3;
+    }
+
+    .item6{
+        grid-column: 1/4;
+
+        .wrapper{
+            width: 440px;
+        }
+        .label, .content, .input{
+            width: 400px;
+        }
+
+        .input, .content{
+            font-size: 1rem;
+        }
+    }
+
+    .label{
+        direction: rtl;
+    }
 `;
