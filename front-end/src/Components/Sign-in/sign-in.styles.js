4988dc7f480817c8c593487cc04007579c04b680
ali varaste pour <alivaraste@outlook.com>
2021-12-21 23:28:06 -0700

    Sign-in RWD (>500px)

--- front-end/src/Components/Sign-in/sign-in.styles.js
+++ front-end/src/Components/Sign-in/sign-in.styles.js
@@ -7,12 +7,18 @@ export const Wrapper = styled.div`
     background-color: #F5F5F5;
     box-shadow: rgba(149, 157, 165, 0.2) 0 8px 24px;
     border-radius: 10px;
+
+    @media only screen and (max-width:695px){
+      &{
+        max-width: 99%;
+      }        
+    }
 `;
 
 export const Content = styled.div`
     max-width: 400px;
     height: 500px;
-    margin: auto;
+    margin: auto; 
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
@@ -20,20 +26,25 @@ export const Content = styled.div`
   *{
     font-family: 'vazir', sans-serif;
   }
+
   .flex-item{
     width: 350px;
   }
+
   .label{
     margin-top: 80px;
     direction: rtl;
     font-size: 1.4rem;
   }
+
   .input{
     margin-top: 20px;
   }
+
   .submit{
     padding-top: 90px;
   }
+
   .inp{
     width: 350px;
     height: 40px;
@@ -43,6 +54,7 @@ export const Content = styled.div`
     font-size: 1.0rem;
     text-align: center;
   }
+
   .button{
     width: 200px;
     height: 50px;
@@ -54,30 +66,42 @@ export const Content = styled.div`
     border: none;
     border-radius: 10px;
   }
+
   .button:hover,  .inp:hover{
     transition: 0.6s;
     background-color: #8b9ce8;
     color: white !important;
   }
+
   .inp:hover::placeholder{
     color: white;
   }
+
   .button:not(:hover),  .inp:not(:hover){
     transition: 0.4s;
     background-color: white;
   }
+
   .error-msg-hide{
     visibility: hidden;
-    
   }
+
   .error-msg-show{
     display: block;
     visibility: visible;
     color: red;
   }
+  
   .errorLogin{
     text-align: center;
     direction: rtl;
   }
+
+  @media only screen and (max-width:695px){
+      &{
+        max-width: 99%;
+        align-content: center;  
+      }
+    }
 
 `;
