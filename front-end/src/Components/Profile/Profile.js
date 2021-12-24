1e38d548db6c387a3a23b959f94862d1a55a5dfb
Ali <alivaraste@outlook.com>
2021-12-24 21:10:57 +0330

    dynamic data loading for profile is added

--- front-end/src/Components/Profile/Profile.js
+++ front-end/src/Components/Profile/Profile.js
@@ -1,17 +1,31 @@
 import { Wrapper } from "./Profile.styles";
+import {profileReducer} from "./Profile.reducer";
 import Field from "../Field/Field";
 import photo from "../../Assets/profile-placeholder.jpg"
 import useGet from "../../custom-hooks/useGet";
-import {useState} from "react";
-
+import {useEffect, useReducer, useState} from "react";
 
 
 const Profile = () => {
 
+
     const URL = 'http://localhost:8000/auth/whoami/' ;
     const TOKEN = sessionStorage.getItem('token') ;
+
     const [edit, setEdit] = useState(false);
-    const {data} = useGet(URL, TOKEN);
+    const data = useGet(URL, TOKEN);
+    const [profileData, dispatch] = useReducer(profileReducer, {});
+
+    useEffect(() => {
+        dispatch({type:'SET-FIRSTNAME', payload:data.firstname});
+        dispatch({type:'SET-LASTNAME', payload:data.lastname});
+        dispatch({type:'SET-EMAIL', payload:data.email});
+        dispatch({type:'SET-ADDRESS', payload:data.address});
+        dispatch({type:'SET-USERNAME', payload:data.username});
+        dispatch({type:'SET-ROLE', payload:data.role});
+    },[data])
+
+    console.log(profileData);
 
     return <>
         <Wrapper>
@@ -23,25 +37,29 @@ const Profile = () => {
             </div>
             <div className="grid-item item2">
                 <div className="label">نام</div>
-                <Field edit={edit} editable={true} content={data.firstname}/>
+                <Field edit={edit} editable={true} onChange={{change:dispatch,type:'SET-FIRSTNAME'}} content={profileData.firstname}/>
             </div>
             <div className="grid-item item3">
                 <div className="label">نام خانوادگی</div>
-                <Field edit={edit} editable={true} content={data.lastname}/>
+                <Field edit={edit} editable={true} onChange={{change:dispatch,type:'SET-LASTNAME'}} content={profileData.lastname}/>
             </div>
             <div className="grid-item item4">
                 <div className="label">کدملی</div>
-                <Field edit={edit} editable={false} content={data.username}/>
+                <Field edit={edit} editable={false} content={profileData.username}/>
             </div>
             <div className="grid-item item5">
                 <div className="label">ایمیل</div>
-                <Field edit={edit} editable={true} content={'تو دو'}/>
+                <Field edit={edit} editable={true} onChange={{change:dispatch,type:'SET-EMAIL'}} content={profileData.email}/>
             </div>
             <div className="grid-item item6">
                 <div className="label">آدرس</div>
-                <Field edit={edit} editable={true} content={data.address}/>
+                <Field edit={edit} editable={true} onChange={{change:dispatch,type:'SET-ADDRESS'}} content={profileData.address}/>
             </div>
             <div className="grid-item item7">
+                <div className="label">نقش</div>
+                <Field edit={edit} editable={false} content={profileData.role}/>
+            </div>
+            <div className="grid-item item8">
                 <button onClick={() => setEdit(!edit)} className='button'>{edit ? 'اعمال تغییرات' : 'تغییر اطلاعات کابری'}</button>
             </div>
         </Wrapper>
