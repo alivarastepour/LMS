a5b48fdc23a38ba653e10f69a5bc54072d2a7398
Ali <alivaraste@outlook.com>
2021-12-24 21:09:34 +0330

    onChange prop is added to Field component

--- front-end/src/Components/Field/Field.js
+++ front-end/src/Components/Field/Field.js
@@ -1,10 +1,10 @@
 import { Wrapper } from "./Field.styles";
 
-const Field = ({edit, content, editable}) => {
+const Field = ({edit, content, editable, onChange}) => {
     return <>
         <Wrapper className="wrapper">
             {
-                edit && editable ? <input spellCheck={false} placeholder={content} className="input" type="text"/>
+                edit && editable ? <input onChange={e => onChange.change({type:onChange.type, payload:e.target.value})} spellCheck={false} placeholder={content} className="input" type="text"/>
                     :
                         <div className="content">{content}</div>
             }
