73f30f3417253c026d34a2254e903e8091ebed6c
ali varaste pour <alivaraste@outlook.com>
2021-12-25 23:49:25 -0700

    lazy loading for mainpage is added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createContext, useState } from "react";
-import MainPage from "./Components/MainPage/MainPage";
-import '../src/Assets/styles/general-styles.css'
-import Dashboard from "./Components/Dashboard/Dashboard";
+import { createContext, useState,lazy, Suspense } from "react";
 
-import Background from "./Components/Background/Background";
+import Setup from "./Components/Spinner/Spinner";
+
+import '../src/Assets/styles/general-styles.css'
+
+const MainPage = lazy(() => import('./Components/MainPage/MainPage'));
+const Dashboard = lazy(() => import('./Components/Dashboard/Dashboard'));
+const Background = lazy(() => import('./Components/Background/Background'));
+
 
 export const authContext = createContext();
 
@@ -12,16 +16,18 @@ const App = () => {
   const [auth, setAuth] = useState(false);
 
   return (
-    <Router>
-    <authContext.Provider value={{auth, setAuth}}>
-      <Routes>
-        <Route path='/' element={<Background component = {<MainPage/>}/>}/>
-        <Route path='/accounts/manager/profile/:id' element={<Background component = {<Dashboard/>}/>}>
-          {/* <Route path=':dashboard' element={<div>hi</div>}/> */}
-        </Route>
-      </Routes>
-    </authContext.Provider>
-    </Router>
+    <Suspense fallback={<Setup/>}>
+      <Router>
+      <authContext.Provider value={{auth, setAuth}}>
+        <Routes>
+          <Route path='/' element={<Background component = {<MainPage/>}/>}/>
+          <Route path='/accounts/manager/profile/:id' element={<Background component = {<Dashboard/>}/>}>
+          </Route>
+        </Routes>
+      </authContext.Provider>
+      </Router>
+    </Suspense>
+
   );
 }
 
