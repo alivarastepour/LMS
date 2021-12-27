e17b52b4296d41d62971f1834728ab010286d1c8
Mahmood Choopani <choopani.m81@gmail.com>
2021-12-27 14:27:00 +0330

    add photo url field.

--- back-end/lms/AuthArena/models.py
+++ back-end/lms/AuthArena/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# TODO: collect all CONSTS to one file
+IMAGE_URL = 'http://localhost/'
+
 
 class CustomUser(AbstractUser):
     role = models.CharField(choices=[('A', 'Admin'),
@@ -8,9 +11,13 @@ class CustomUser(AbstractUser):
                                      ('T', 'Teacher'),
                                      ('S', 'Student')],
                             max_length=1)
-    address = models.CharField(max_length=512, null=True, blank=True,default='')
+    address = models.CharField(max_length=512, null=True, blank=True, default='')
+    photo_link = models.URLField(default=IMAGE_URL + 'default.jpg')
 
-    # TODO: add picture
     @property
     def fullname(self):
         return self.first_name + ' ' + self.last_name
+
+    def set_photo_link(self, name):
+        self.photo_link = IMAGE_URL + name
+        self.save()
