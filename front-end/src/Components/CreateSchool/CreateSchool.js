4b589107851005bd4c91abfc94dd52a2c717a84e
Ali <alivaraste@outlook.com>
2021-12-29 18:39:58 +0330

    CreateSchool component is added

---
+++ front-end/src/Components/CreateSchool/CreateSchool.js
@@ -0,0 +1,10 @@
+import {Wrapper} from "./CreateSchool.styles";
+
+const CreateSchool = () => {
+    return <>
+        <Wrapper>
+            hello world
+        </Wrapper>
+    </>
+}
+export default CreateSchool;
