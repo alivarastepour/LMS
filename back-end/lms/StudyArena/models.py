cc00d099eb04e5e86e970c35c00c5fbc7ece9bd6
Mahmood Choopani <choopani.m81@gmail.com>
2021-12-29 10:55:06 +0330

    /study/school/ endpoint wrote to GET manager schools and POST new school.

--- back-end/lms/StudyArena/models.py
+++ back-end/lms/StudyArena/models.py
@@ -1,12 +1,26 @@
 from django.db import models
+from AuthArena.models import CustomUser
 
 
 class School(models.Model):
     school_id = models.CharField(max_length=10)
     name = models.CharField(max_length=100, null=False, blank=False)
     address = models.CharField(max_length=500, null=True)
+    # TODO: change manager relation to : one to many
+    #  OR
+    #  delete denied School after save new school
+    manager = models.OneToOneField(CustomUser, on_delete=models.DO_NOTHING)
     accepted = models.BooleanField(default=False)
     denied = models.BooleanField(default=False)
+
+    def to_json(self):
+        return {
+            'school_id': self.school_id,
+            'name': self.school_id,
+            'address': self.address,
+            'accepted': self.accepted,
+            'denied': self.denied
+        }
 
 
 class Class(models.Model):
--- back-end/lms/StudyArena/views.py
+++ back-end/lms/StudyArena/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
-from .permissions import IsManager
+from .models import School
+from .permissions import IsManager, IsProfileCompleted
 from rest_framework.views import APIView
 from .serilizers import SchoolSerializer
 
 
-class NewSchool(APIView):
+class SchoolView(APIView):
     serializer_class = SchoolSerializer
-    permission_classes = (IsAuthenticated, IsManager)
+    permission_classes = (IsAuthenticated, IsProfileCompleted, IsManager)
 
     def post(self, request):
         school = SchoolSerializer(data=request.data)
         if school.is_valid():
-            school.save()
+            school.save(manager=request.user)
             return Response(data={'message': f'{request.data["name"]} successfully created.'}, status=200)
         return Response(data={'message': 'some fields are missing.', 'errors': school.errors}, status=400)
 
+    def get(self, request):
+        school = get_object_or_404(School, manager__id=request.user.id)
+        return Response(data=school.to_json())
