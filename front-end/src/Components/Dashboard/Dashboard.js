c808dedfbdd558f78f2ae987dd16b2e73d5f51db
ali varaste pour <alivaraste@outlook.com>
2021-12-29 02:48:21 -0700

    improved routing

--- front-end/src/Components/Dashboard/Dashboard.js
+++ front-end/src/Components/Dashboard/Dashboard.js
@@ -1,34 +1,21 @@
-import {useContext, useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useParams} from "react-router-dom";
 
 import { Wrapper } from "./Dashboard.styles";
 import Profile from "../Profile/Profile";
 import DashboardHeader from "./Dashboard.header";
 
-import {authContext} from "../../App";
-
-
 const Dashboard = () => {
 
-    const navigator = useNavigate();
+    const defaultPage = useParams().profile;
 
-    const nav = () => navigator(`/`);
-
-    const [state, setState] = useState(true);
-
-    const {auth, setAuth} = useContext(authContext);
-
-    // useEffect(() => {
-    //     if (!auth) {
-    //         nav();
-    //     }
-    // });
+    const [show, setShow] = useState(defaultPage !== 'managment');
 
     return <>
         <Wrapper>
-            <DashboardHeader show={state} setShow={setState}/>
+            <DashboardHeader show={show} setShow={setShow}/>
             {
-                state ? <Profile/> : <div>hi there</div>
+                show ? <Profile/> : <div>hi there</div>
             }
         </Wrapper>
     </>
