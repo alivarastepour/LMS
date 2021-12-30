0aaf918821c6559176eea4b9b686a38fc61f7be7
ali varaste pour <alivaraste@outlook.com>
2021-12-30 08:58:31 -0700

    SchoolStatus component and relevant styles are added

---
+++ front-end/src/Components/SchoolStatus/SchoolStatus.js
@@ -0,0 +1,20 @@
+import { Wrapper } from "./SchoolStatus.styles";
+import Spinner from "../Spinner/Spinner";
+const SchoolStatus = ({status}) => {
+    return <> 
+        <Wrapper>
+
+    {
+        status === 'pending' ? 
+        <Spinner color={{c:'#8b9ce8'}} text='درخواست شما در دست بررسی است. از شکیبایی شما متشکریم.'/>
+        :
+        <div>
+        <div className="error">متاسفانه درخواست شما توسط مدیریت سایت مورد پذیرش واقع نشده است.</div>
+        <button className="button">درخواست مجدد</button>
+        </div>
+        
+    }
+        </Wrapper>
+    </>
+}
+export default SchoolStatus;
