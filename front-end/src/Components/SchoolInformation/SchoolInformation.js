a5bdde16d97a4341ecc018db21c3f71a97542c09
ali varaste pour <alivaraste@outlook.com>
2021-12-31 00:02:34 -0700

    SchoolInformation Component is added

---
+++ front-end/src/Components/SchoolInformation/SchoolInformation.js
@@ -0,0 +1,12 @@
+import { Wrapper, Content } from "./SchoolInformation.styles";
+import SchoolInformationHeader from "./SchoolInformation.Header";
+const SchoolInformation = () => {
+    return <>
+        <Wrapper>
+            <SchoolInformationHeader/>
+            <Content/>
+        </Wrapper>
+    </>
+}
+
+export default SchoolInformation;
---
+++ front-end/src/Components/SchoolInformation/SchoolInformation.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
+export const Content = styled.div`
+    width: 90%;
+    height: calc(100% - 50px);
+    background-color: 	#E8E8E8;
+    border-radius: 10px 0 10px 10px;
+    margin: auto;
+`;
