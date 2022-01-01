a11850ead19dd3cdbe3ba737d51c749d3f0b9f4a
ali varaste pour <alivaraste@outlook.com>
2022-01-01 10:48:17 -0700

    render logic for SchoolProfile is added

--- front-end/src/Components/SchoolInformation/SchoolInformation.js
+++ front-end/src/Components/SchoolInformation/SchoolInformation.js
@@ -1,15 +1,16 @@
 import { Wrapper, Content } from "./SchoolInformation.styles";
 import SchoolInformationHeader from "./SchoolInformation.Header";
 import TableView from "../TableView/TableView";
+import SchoolProfile from "../SchoolProfile/SchoolProfile";
 import { useState } from "react";
 const SchoolInformation = () => {
-    const [state, setState] = useState('student');
+    const [state, setState] = useState('info');
     return <>
         <Wrapper>
             <SchoolInformationHeader state={state} setState={setState}/>
             <Content>
                 {
-                    state === 'student' ? <TableView/> : <div>to be completed</div>
+                    state === 'student' ? <TableView/> : state === 'info' ? <SchoolProfile/> : <h1>salam</h1>
                 }
             </Content>
         </Wrapper>
