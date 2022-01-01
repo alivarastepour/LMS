007b836433606144f8d5b401266a6af38349e178
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-01 18:01:48 +0330

    /study/student-list/ endpoint added to get all requested students of a school.

--- back-end/lms/StudyArena/urls.py
+++ back-end/lms/StudyArena/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SchoolView
+from .views import SchoolView,StudentRequests
 
 urlpatterns = [
-    path('school/', SchoolView.as_view())
+    path('school/', SchoolView.as_view()),
+    path('student-list/', StudentRequests.as_view())
 ]
--- back-end/lms/StudyArena/views.py
+++ back-end/lms/StudyArena/views.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import School
-from .permissions import IsManager, IsProfileCompleted
+from .permissions import IsManager, IsProfileCompleted, IsTeacher
 from rest_framework.views import APIView
 from .serilizers import SchoolSerializer
 
@@ -27,3 +27,31 @@ class SchoolView(APIView):
             }, status=200)
         res_dic = school.to_json()
         return Response(data=res_dic)
+
+
+class StudentRequests(APIView):
+    permission_classes = (IsAuthenticated, IsProfileCompleted, IsManager | IsTeacher)
+
+    def get(self, request):
+
+        all_classes = request.user.school.class_set.all()
+
+        output = []
+        for cls in all_classes:
+            class_requests = cls.studentrequest_set.all()
+            for req in class_requests:
+                output.append(
+                    {
+                        'student': {
+                            'name': req.student.user.fullname,
+                            'email': req.student.user.email,
+                            'photo': req.student.user.photo_link
+                        },
+                        'class': {
+                            'name': req.clazz.name,
+                            'school_name': req.clazz.school.name
+                        },
+                        'status': req.status
+                    }
+                )
+        return Response(data={'requests': output}, status=200)
