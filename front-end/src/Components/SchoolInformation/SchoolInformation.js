0565b6a96e38a3718c722c961af8b03ceddca5d5
ali varaste pour <alivaraste@outlook.com>
2022-01-02 11:20:47 -0700

    render logic is improved

--- front-end/src/Components/SchoolInformation/SchoolInformation.js
+++ front-end/src/Components/SchoolInformation/SchoolInformation.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
+
 import { Wrapper, Content } from "./SchoolInformation.styles";
 import SchoolInformationHeader from "./SchoolInformation.Header";
 import TableView from "../TableView/TableView";
 import SchoolProfile from "../SchoolProfile/SchoolProfile";
-import { useState } from "react";
+
 const SchoolInformation = () => {
+
     const [state, setState] = useState('info');
+
     return <>
         <Wrapper>
             <SchoolInformationHeader state={state} setState={setState}/>
             <Content>
                 {
-                    state === 'student' ? <TableView/> : state === 'info' ? <SchoolProfile/> : <h1>salam</h1>
+                    state === 'student' ? 
+                    <TableView content='student'/> : 
+                    state === 'info' ? 
+                    <SchoolProfile/> : 
+                    <TableView content='teacher'/>
                 }
             </Content>
         </Wrapper>
