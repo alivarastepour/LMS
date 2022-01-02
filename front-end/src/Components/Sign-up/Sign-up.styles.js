8504387de56c301ed45dac3676dc482676d8be27
ali varaste pour <alivaraste@outlook.com>
2022-01-02 03:06:59 -0700

    button shadow is added

--- front-end/src/Components/Sign-up/Sign-up.styles.js
+++ front-end/src/Components/Sign-up/Sign-up.styles.js
@@ -68,6 +68,7 @@ export const Content = styled.div`
     background-color: white;
     border: none;
     border-radius: 10px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
   }
 
   .button:hover, .inp:hover {
