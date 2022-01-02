98491f6b53855f297bb3d374193585cf95750339
ali varaste pour <alivaraste@outlook.com>
2022-01-02 11:20:23 -0700

    data loading for school profile is added

--- front-end/src/Components/SchoolProfile/SchoolProfile.js
+++ front-end/src/Components/SchoolProfile/SchoolProfile.js
@@ -1,24 +1,33 @@
 import { Wrapper } from "./SchoolProfile.styles";
 import Field from "../Field/Field";
 import photo from '../../Assets/profile-placeholder.jpg'
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { schoolProfileReducer } from "./schoolProfile.reducer";
 import useGet from "../../custom-hooks/useGet";
 
 const SchoolProfile = () => {
-    const URL = 'http://localhost:8000/study/school/';
-    const TOKEN = sessionStorage.getItem('token');
 
-    // const temp = useGet(URL,TOKEN);
-    // console.log(temp);
     const initialState = {
         schoolName:'',
         schoolId:'',
         schoolAddress:''
     }
+    
+    const URL = 'http://localhost:8000/study/school/';
+    const TOKEN = sessionStorage.getItem('token');
+
+    const {data} = useGet(URL, TOKEN);
+
+
     const [state, setState] = useState(false);
 
-    const [data, dispatch] = useReducer(schoolProfileReducer,initialState);
+    const [info, dispatch] = useReducer(schoolProfileReducer,initialState);
+
+    useEffect(() => {
+        dispatch({type:'SET-SCHOOL-ADDRESS',payload:data.address});
+        dispatch({type:'SET-SCHOOL-ID',payload:data.school_id});
+        dispatch({type:'SET-SCHOOL-NAME',payload:data.name});
+    },[data])
 
     return <>
         <Wrapper>
@@ -28,20 +37,34 @@ const SchoolProfile = () => {
             </div>
             <div className="flex-item content">
                 <div className="label">شناسه مدرسه</div>
-                <Field edit={state} content={data.schoolId} editable={false}/>
+                <Field
+                edit={state}
+                content={info.schoolId}
+                editable={false}/>
             </div>
             <div className="flex-item content">
                 <div className="label">نام مدرسه</div>
-                <Field edit={state} content={data.schoolName} editable={true} onChange={{change:dispatch, type:'SET-SCHOOL-NAME'}}/>
+                <Field
+                edit={state} 
+                content={info.schoolName} 
+                editable={true} 
+                onChange={{change:dispatch, type:'SET-SCHOOL-NAME'}}/>
             </div>
             <div className="flex-item content">
                 <div className="label">آدرس مدرسه</div>
-                <Field edit={state} content={data.schoolAddress} editable={true} onChange={{change:dispatch, type:'SET-SCHOOL-ADDRESS'}}/>
+                <Field 
+                edit={state} 
+                content={info.schoolAddress} 
+                editable={true} 
+                onChange={{change:dispatch, type:'SET-SCHOOL-ADDRESS'}}/>
             </div>
             <div className="flex-item content change">
-                <button onClick={() => setState((prev) => setState(!prev))} className="button">{state ? 'ثبت تغییرات' : 'تغییر اطلاعات'}</button>
+                <button
+                onClick={() => setState((prev) => setState(!prev))} 
+                className="button">{state ? 'ثبت تغییرات' : 'تغییر اطلاعات'}</button>
             </div>
         </Wrapper>
     </>
 }
+
 export default SchoolProfile;
