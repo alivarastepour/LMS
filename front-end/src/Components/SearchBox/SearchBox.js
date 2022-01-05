0e4abb173fba11c6ad0ffb4a320e1b3ec3117c69
ali varaste pour <alivaraste@outlook.com>
2022-01-05 03:05:13 -0700

    SearchBox component is created

---
+++ front-end/src/Components/SearchBox/SearchBox.js
@@ -0,0 +1,30 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment, TextField} from "@mui/material";
+import { Wrapper } from './SearchBox.styles';
+
+const SearchBox = ({searchValue, setSearchValue, handleSearch, staticInformation, setInformation, searchTerm, placeHolder}) => {
+    return <>
+    <Wrapper>
+        <TextField
+        InputProps={{
+            startAdornment: (
+                <InputAdornment position="start">
+                    <SearchIcon />
+                </InputAdornment>
+            ),
+        }}
+        size="small"
+        sx={{width:380,boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',background:'#F8F8F8'}}
+        label='جستجو'
+        placeholder={placeHolder}
+        value={searchValue}
+        onChange={(e) => {
+            setSearchValue(e.target.value)
+            handleSearch(e,staticInformation, setInformation, searchTerm);
+        }}
+        type='text'/>
+        </Wrapper>
+    </>
+}
+
+export default SearchBox ;
---
+++ front-end/src/Components/SearchBox/SearchBox.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+
+.css-1pysi21-MuiFormLabel-root-MuiInputLabel-root,
+  .css-1ua80n0-MuiInputBase-input-MuiOutlinedInput-input,
+  .css-1kty9di-MuiFormLabel-root-MuiInputLabel-root.Mui-focused,
+  .css-1kty9di-MuiFormLabel-root-MuiInputLabel-root,
+  input{
+    font-family: 'vazir', sans-serif;
+  }
+`;
