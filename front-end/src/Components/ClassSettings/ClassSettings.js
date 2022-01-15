bd8d835a20f37c1996a726a4fcf81861efe46533
ali varaste pour <alivaraste@outlook.com>
2022-01-15 09:15:37 -0700

    controlled inputs

--- front-end/src/Components/ClassSettings/ClassSettings.js
+++ front-end/src/Components/ClassSettings/ClassSettings.js
@@ -1,23 +1,65 @@
 import { Wrapper } from "./ClassSettings.styles";
 import { Switch } from "@mui/material";
+import { ClassSettingsReducer } from "./classSettings.reducer";
+import { useReducer } from "react";
 const ClassSettings = () => {
+  const fakeData = {
+    name: "کلاس اول گل‌ها",
+    id: "salamp",
+    msg: "به کلاس گلهای لاله خوش آمدید",
+    maxAttendees: 32,
+    duration: 100,
+    rec: true,
+    autoRec: true,
+    pauseRec: true,
+    msgAttendees: true,
+    wcAttendees: true,
+  };
+  const [value, dispatch] = useReducer(ClassSettingsReducer, fakeData);
   return (
     <>
       <Wrapper>
         <label className="label" htmlFor="name">
           نام کلاس
         </label>
-        <input id="name" className="input" type="text" placeholder="" />
+        <input
+          id="name"
+          className="input"
+          type="text"
+          placeholder={value.name}
+          value={value.name}
+          onChange={(e) =>
+            dispatch({ type: "SET-CLASS-NAME", payload: e.target.value })
+          }
+        />
 
         <label className="label" htmlFor="id">
           شناسه کلاس
         </label>
-        <input id="id" className="input" type="text" placeholder="" />
+        <input
+          id="id"
+          className="input"
+          type="text"
+          onChange={(e) =>
+            dispatch({ type: "SET-CLASS-ID", payload: e.target.value })
+          }
+          placeholder={value.id}
+          value={value.id}
+        />
 
         <label className="label" htmlFor="msg">
           پیام خوش‌آمد
         </label>
-        <input id="msg" className="input" type="text" placeholder="" />
+        <input
+          id="msg"
+          className="input"
+          type="text"
+          onChange={(e) =>
+            dispatch({ type: "SET-CLASS-WELCOME-MSG", payload: e.target.value })
+          }
+          placeholder={value.msg}
+          value={value.msg}
+        />
 
         <label className="label" htmlFor="">
           حداکثر تعداد شرکت‌کنندگان
@@ -26,7 +68,14 @@ const ClassSettings = () => {
           id="max-attendees"
           className="input"
           type="number"
-          placeholder=""
+          placeholder={value.maxAttendees}
+          onChange={(e) =>
+            dispatch({
+              type: "SET-CLASS-MAX-ATTENDEES",
+              payload: e.target.value,
+            })
+          }
+          value={value.maxAttendees}
         />
 
         <label className="label" htmlFor="duration">
@@ -38,42 +87,90 @@ const ClassSettings = () => {
           type="number"
           min={1}
           max={120}
-          placeholder=""
+          placeholder={value.duration}
+          onChange={(e) =>
+            dispatch({ type: "SET-CLASS-DURATION", payload: e.target.value })
+          }
+          value={value.duration}
         />
         <div>
           <label className="label switch" htmlFor="rec">
             قابلیت ضبط
           </label>
 
-          <Switch id="rec" />
+          <Switch
+            checked={value.rec}
+            onChange={(e) =>
+              dispatch({ type: "SET-CLASS-RECORD", payload: e.target.checked })
+            }
+            id="rec"
+          />
         </div>
         <div>
           <label className="label switch" htmlFor="auto-rec">
             شروع خودکار ضبط
           </label>
 
-          <Switch id="auto-rec" />
+          <Switch
+            checked={value.autoRec}
+            onChange={(e) =>
+              dispatch({
+                type: "SET-CLASS-AUTO-RECORD",
+                payload: e.target.checked,
+              })
+            }
+            disabled={!value.rec}
+            id="auto-rec"
+          />
         </div>
         <div>
           <label className="label switch" htmlFor="pause-rec">
             توقف/ادامه ضبط
           </label>
 
-          <Switch id="pause-rec" />
+          <Switch
+            checked={value.pauseRec}
+            disabled={!value.rec}
+            id="pause-rec"
+            onChange={(e) =>
+              dispatch({
+                type: "SET-CLASS-PAUSE-RECORD",
+                payload: e.target.checked,
+              })
+            }
+          />
         </div>
         <div>
           <label className="label switch" htmlFor="pm">
             پیام شرکت کننده
           </label>
 
-          <Switch id="pm" />
+          <Switch
+            onChange={(e) =>
+              dispatch({
+                type: "SET-CLASS-MSG-ATTENDEES",
+                payload: e.target.checked,
+              })
+            }
+            checked={value.msgAttendees}
+            id="pm"
+          />
         </div>
         <div>
           <label className="label switch" htmlFor="wc">
             وب‌کم شرکت کننده
           </label>
 
-          <Switch id="wc" />
+          <Switch
+            onChange={(e) =>
+              dispatch({
+                type: "SET-CLASS-WC-ATTENDEES",
+                payload: e.target.checked,
+              })
+            }
+            checked={value.wcAttendees}
+            id="wc"
+          />
         </div>
 
         <button className="button acc">ذخیره</button>
