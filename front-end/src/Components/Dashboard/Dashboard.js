7e92749505be9312d8c004d9c1904afbb75d25f8
ali varaste pour <alivaraste@outlook.com>
2022-01-16 05:49:39 -0700

    authentication check is added

--- front-end/src/Components/Dashboard/Dashboard.js
+++ front-end/src/Components/Dashboard/Dashboard.js
@@ -1,8 +1,9 @@
-import { useState, lazy, Suspense } from "react";
-import { useParams } from "react-router-dom";
+import { useState, lazy, Suspense, useContext, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { Wrapper } from "./Dashboard.styles";
 import Spinner from "../Spinner/Spinner";
+import { authContext } from "../../App";
 
 const Profile = lazy(() => import("../Profile/Profile"));
 const DashboardHeader = lazy(() => import("./Dashboard.header"));
@@ -12,9 +13,21 @@ const SchoolManagement = lazy(() =>
 const Footer = lazy(() => import("../Footer/Footer"));
 
 const Dashboard = () => {
+  const nav = () => navigator("/");
+
   const defaultPage = useParams().profile;
 
   const [show, setShow] = useState(defaultPage !== "management");
+
+  const { auth } = useContext(authContext);
+
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    if (!auth) {
+      nav();
+    }
+  }, [auth, nav]);
 
   return (
     <>
