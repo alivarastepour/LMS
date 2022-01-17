a4b434fdf77f265907fb25900ebc3da33b6ea8f9
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-17 13:13:54 +0330

    add an endpoint to get a class settings.

--- back-end/lms/StudyArena/urls.py
+++ back-end/lms/StudyArena/urls.py
@@ -4,6 +4,7 @@ from .views import SchoolView, StudentRequests, TeacherRequests, ClassView
 urlpatterns = [
     path('school/', SchoolView.as_view()),
     path('class/', ClassView.as_view()),
+    path('class/<int:class_id>/', ClassView.as_view()),
     path('student-list/', StudentRequests.as_view()),
     path('teacher-list/', TeacherRequests.as_view()),
     path('teacher-request/<int:teacher_id>/', TeacherRequests.as_view()),
