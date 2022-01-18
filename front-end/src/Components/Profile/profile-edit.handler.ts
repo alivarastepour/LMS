7f57c7616f32f4189e203bd779fb4f4c87ec5df7
ali varaste pour <alivaraste@outlook.com>
2022-01-18 03:39:01 -0700

    Tprofile type is added

--- front-end/src/Components/Profile/profile-edit.handler.ts
+++ front-end/src/Components/Profile/profile-edit.handler.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
 
+type Tprofile = {
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  role: string;
+  address: string;
+  image: string;
+};
+
 export const profileEditHandler = (
   edit: boolean,
   setEdit: Function,
-  state: {
-    username: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    role: string;
-    address: string;
-    image: string;
-  }
+  state: Tprofile
 ) => {
   const URL = "http://localhost:8000/auth/whoami/";
   const TOKEN = sessionStorage.getItem("token");
