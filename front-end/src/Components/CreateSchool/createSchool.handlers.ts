1d0493d065aee122796e387b703b6d0abf662f24
ali varaste pour <alivaraste@outlook.com>
2022-01-18 03:37:49 -0700

    Tschool type is added

--- front-end/src/Components/CreateSchool/createSchool.handlers.ts
+++ front-end/src/Components/CreateSchool/createSchool.handlers.ts
@@ -1,18 +1,21 @@
 import React from "react";
 import axios from "axios";
 
+type Tschool = {
+  schoolId: string;
+  schoolName: string;
+  schoolAddress: string;
+};
+
 export const createSchoolHandler = async (
   event: React.FormEvent<HTMLFormElement>,
-  data: { schoolId: string; schoolName: string; schoolAddress: string }
+  data: Tschool
 ) => {
+  const URL = "http://localhost:8000/study/school";
   const TOKEN = sessionStorage.getItem("token");
-
   await axios
-    .post("http://localhost:8000/study/school", data, {
+    .post(URL, data, {
       headers: { Authorization: `Token ${TOKEN}` },
-    })
-    .then((res) => {
-      console.log(res);
     })
     .catch((e) => {
       console.log(e);
