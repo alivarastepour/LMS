2a11d9a4d59383a4651d026d664244e4c29bf7ab
ali varaste pour <alivaraste@outlook.com>
2022-01-19 02:52:26 -0700

    useEffect callback is wrapped in useCallback

--- front-end/src/Components/Dashboard/Dashboard.js
+++ front-end/src/Components/Dashboard/Dashboard.js
@@ -1,27 +1,35 @@
-import { useState, lazy, Suspense, useContext, useEffect } from "react";
+import {
+  useState,
+  lazy,
+  Suspense,
+  useContext,
+  useEffect,
+  useCallback,
+} from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { Wrapper } from "./Dashboard.styles";
 import Spinner from "../Spinner/Spinner";
-import { authContext } from "../../App";
 import AccessibilityIdentifier from "./AccessibilityIdentifier";
+
+import { authContext } from "../../App";
 
 const Profile = lazy(() => import("../Profile/Profile"));
 const DashboardHeader = lazy(() => import("./Dashboard.header"));
-
 const Footer = lazy(() => import("../Footer/Footer"));
 
 const Dashboard = () => {
-  const nav = () => navigator("/");
-  const role = "student";
+  const navigator = useNavigate();
 
-  const defaultPage = useParams().profile;
+  const nav = useCallback(() => navigator("/"), [navigator]);
+
+  const role = "manager";
+
+  const defaultPage = useParams().management;
 
   const [show, setShow] = useState(defaultPage !== "management");
 
   const { auth } = useContext(authContext);
-
-  const navigator = useNavigate();
 
   useEffect(() => {
     if (!auth) {
