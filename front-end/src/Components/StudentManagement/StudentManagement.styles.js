5e75da95c42e73c3ae59e60e4d40808a57bb7de5
ali varaste pour <alivaraste@outlook.com>
2022-01-21 00:31:06 -0700

    styles are moved to currect file

--- front-end/src/Components/StudentManagement/StudentManagement.styles.js
+++ front-end/src/Components/StudentManagement/StudentManagement.styles.js
@@ -5,42 +5,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   overflow-y: auto;
   width: 90%;
-  height: 100%;
+  height: 90%;
   background-color: #edf0ff;
   border-radius: 10px;
-
-  .list-title {
-    font-family: "vazir", sans-serif;
-    font-size: 1.3rem;
-    direction: rtl;
-    padding-right: 10px;
-  }
-
-  .list-item {
-    font-family: "vazir", sans-serif;
-  }
-
-  .p {
-    margin-right: 20px;
-  }
-
-  .no-class {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-    margin: auto;
-    direction: rtl;
-    font-family: "vazir", sans-serif;
-    font-size: 1.5rem;
-
-    &:hover {
-      transition: 0.4s;
-      color: #4456b1;
-    }
-    &:not(:hover) {
-      transition: 0.4s;
-      color: black;
-    }
-  }
 `;
--- front-end/src/Components/StuedntClasses/StudentClasses.styles.js
+++ front-end/src/Components/StuedntClasses/StudentClasses.styles.js
@@ -3,4 +3,28 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+
+  .list-item {
+    font-family: "vazir", sans-serif;
+  }
+
+  .no-class {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    margin: auto;
+    direction: rtl;
+    font-family: "vazir", sans-serif;
+    font-size: 1.5rem;
+
+    &:hover {
+      transition: 0.4s;
+      color: #4456b1;
+    }
+    &:not(:hover) {
+      transition: 0.4s;
+      color: black;
+    }
+  }
 `;
