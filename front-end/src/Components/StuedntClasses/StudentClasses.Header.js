038933c1caf2511683d009940e9ee82d461ab36f
ali varaste pour <alivaraste@outlook.com>
2022-01-21 00:30:31 -0700

    header for StudentClass component is added

---
+++ front-end/src/Components/StuedntClasses/StudentClasses.Header.js
@@ -0,0 +1,50 @@
+import { Wrapper } from "./StudentClasses.Header.styles";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+const StudentClassesHeader = ({ state, setState, classes }) => {
+  return (
+    <>
+      <Wrapper>
+        <div className="list-title">
+          <div>لیست کلاس ها</div>
+          <div>
+            <FormControl>
+              <InputLabel
+                sx={{
+                  fontFamily: "vazir",
+                  fontSize: "1.1rem",
+                }}
+                id="select"
+              >
+                یک مدرسه را انتخاب کنید
+              </InputLabel>
+              <Select
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+                sx={{ width: 400 }}
+                labelId="select"
+                id="select"
+                label="یک مدرسه را انتخاب کنید"
+              >
+                {classes.map((e) => (
+                  <MenuItem
+                    value={e}
+                    key={e}
+                    sx={{
+                      textAlign: "right",
+                      direction: "rtl",
+                      fontFamily: "vazir",
+                    }}
+                  >
+                    <div className="select-item">{e}</div>
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+        </div>
+      </Wrapper>
+    </>
+  );
+};
+export default StudentClassesHeader;
---
+++ front-end/src/Components/StuedntClasses/StudentClasses.Header.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  .select-item,
+  .css-1kty9di-MuiFormLabel-root-MuiInputLabel-root {
+    font-family: "vazir", sans-serif;
+  }
+  .list-title {
+    font-family: "vazir", sans-serif;
+    font-size: 1.5rem;
+    direction: rtl;
+    padding-right: 10px;
+    padding-top: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+`;
