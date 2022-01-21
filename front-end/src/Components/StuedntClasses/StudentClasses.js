be05acbf6d219fb6dcd2c8ced233676c2dd7b3ea
ali varaste pour <alivaraste@outlook.com>
2022-01-21 00:31:20 -0700

    refactor

--- front-end/src/Components/StuedntClasses/StudentClasses.js
+++ front-end/src/Components/StuedntClasses/StudentClasses.js
@@ -2,35 +2,46 @@ import { Wrapper } from "./StudentClasses.styles";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
 import List from "@mui/material/List";
+import { useState } from "react";
+import StudentClassesHeader from "./StudentClasses.Header";
+const items = [
+  "درس۱",
+  "درس۲",
+  "درس۳",
+  "درس۴",
+  "درس۵",
+  "درس۶",
+  "درس۷",
+  "درس۸",
+  "درس۹",
+  "درس۱۰",
+];
+const classes = ["مدرسه۱", "مدرسه۲", "مدرس۳", "مدرسه۴", "پالام پولوم پیلیش"];
+const listItemStyles = {
+  direction: "rtl",
+  textAlign: "right",
+};
+const listStyles = {
+  width: "95%",
+  margin: "auto",
+  bgcolor: "white",
+  borderRadius: 5,
+};
+
 const StudentClasses = () => {
-  const items = [
-    "درس۱",
-    "درس۲",
-    "درس۳",
-    "درس۴",
-    "درس۵",
-    "درس۶",
-    "درس۷",
-    "درس۸",
-    "درس۹",
-    "درس۱۰",
-  ];
-  const listItemStyles = {
-    direction: "rtl",
-    textAlign: "right",
-  };
-  const listStyles = {
-    width: "95%",
-    margin: "auto",
-    bgcolor: "white",
-    borderRadius: 5,
-  };
+  const [state, setState] = useState("");
   return (
     <>
       <Wrapper>
         <List
           sx={listStyles}
-          subheader={<div className="list-title">لیست کلاس ها</div>}
+          subheader={
+            <StudentClassesHeader
+              state={state}
+              setState={setState}
+              classes={classes}
+            />
+          }
         >
           {items.length !== 0 ? (
             items.map((e) => {
