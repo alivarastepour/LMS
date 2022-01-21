dbcaf56a7a22618a263376d1dffc2a21131461f7
ali varaste pour <alivaraste@outlook.com>
2022-01-20 23:29:34 -0700

    StudentManagement and relavant styles are added

--- front-end/src/Components/StudentManagement/StudentManagement.js
+++ front-end/src/Components/StudentManagement/StudentManagement.js
@@ -1,11 +1,26 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
 import List from "@mui/material/List";
 
 import { Wrapper } from "./StudentManagement.styles";
+import StudentManagementHeader from "./StudentManagement.Header";
+import { Content } from "../SchoolInformation/SchoolInformation.styles";
+
+const items = [
+  // "درس۱",
+  // "درس۲",
+  // "درس۳",
+  // "درس۴",
+  // "درس۵",
+  // "درس۶",
+  // "درس۷",
+  // "درس۸",
+  // "درس۹",
+  // "درس۱۰",
+];
 
 const StudentManagement = () => {
   const listItemStyles = {
@@ -14,42 +29,42 @@ const StudentManagement = () => {
   };
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const defaultPage = useParams().info;
+  const [state, setState] = useState(defaultPage);
   const nav = () => navigate(`./randomeShit`);
   return (
     <>
       <Wrapper>
-        <List
-          sx={{
-            width: "95%",
-            margin: "auto",
-            bgcolor: "white",
-            borderRadius: 5,
-          }}
-          subheader={<div className="list-title">لیست کلاس ها</div>}
-        >
-          <ListItemButton>
-            <ListItemText sx={listItemStyles}>
-              <div className="list-item">مدار های الکتریکی و الکترونیکی</div>
-            </ListItemText>
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemText sx={listItemStyles}>
-              <div className="list-item">مدار های منطقی</div>
-            </ListItemText>
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemText sx={listItemStyles}>
-              <div className="list-item" onClick={() => nav()}>
-                ساختمان داده
-              </div>
-            </ListItemText>
-          </ListItemButton>
-          <ListItemButton>
-            <ListItemText sx={listItemStyles}>
-              <div className="list-item">عرفان عملی در اسلام</div>
-            </ListItemText>
-          </ListItemButton>
-        </List>
+        <StudentManagementHeader state={state} setState={setState} />
+        <Content>
+          {state === "info" ? (
+            items.length !== 0 ? (
+              <List
+                sx={{
+                  width: "95%",
+                  margin: "auto",
+                  bgcolor: "white",
+                  borderRadius: 5,
+                }}
+                subheader={<div className="list-title">لیست کلاس ها</div>}
+              >
+                {items.map((e) => {
+                  return (
+                    <ListItemButton key={e}>
+                      <ListItemText sx={listItemStyles}>
+                        <div className="list-item">{e}</div>
+                      </ListItemText>
+                    </ListItemButton>
+                  );
+                })}
+              </List>
+            ) : (
+              <div className="no-class">شما کلاسی ندارید.</div>
+            )
+          ) : (
+            <div className="no-class">کلاسا مدرسه</div>
+          )}
+        </Content>
       </Wrapper>
     </>
   );
