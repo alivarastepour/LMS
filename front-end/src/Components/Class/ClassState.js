47997a92b7aa1107e4dfc59120dba6f7c623773a
ali varaste pour <alivaraste@outlook.com>
2022-02-08 10:19:41 -0700

    ClassState component is added(code separation)

---
+++ front-end/src/Components/Class/ClassState.js
@@ -0,0 +1,60 @@
+import ContentCopyTwoToneIcon from "@mui/icons-material/ContentCopyTwoTone";
+import { TextField, InputAdornment, IconButton, Tooltip } from "@mui/material";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+
+const ClassState = ({ url, setOpen, status }) => {
+  const classStatus = () => {
+    if (status) {
+      return <div className="status on">در حال برگزاری</div>;
+    } else {
+      return <div className="status off">شروع نشده</div>;
+    }
+  };
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(url);
+  };
+  return (
+    <>
+      <div className="state">
+        <div className="state-container">
+          <div style={{ display: "inline-block" }}>وضعیت کلاس:</div>
+          {classStatus()}
+        </div>
+        <div className="link-container">
+          <div className="enter">لینک ورود به کلاس</div>
+          <div>
+            <TextField
+              InputProps={{
+                readOnly: true,
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Tooltip title="copy class link to clipboard">
+                      <IconButton
+                        onClick={() => {
+                          setOpen(true);
+                          copyToClipboard();
+                        }}
+                      >
+                        <ContentCopyTwoToneIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="open class">
+                      <IconButton disabled={false} onClick={() => {}}>
+                        <OpenInNewIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </InputAdornment>
+                ),
+              }}
+              size="small"
+              sx={{ width: "800px", direction: "ltr" }}
+              defaultValue={url}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ClassState;
