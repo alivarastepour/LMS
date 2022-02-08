a67748110e708d2a3ae3413fd1b28589d835a5e8
ali varaste pour <alivaraste@outlook.com>
2022-02-08 06:34:04 -0700

    routing to class page is added

--- front-end/src/Components/StuedntClasses/StudentClasses.js
+++ front-end/src/Components/StuedntClasses/StudentClasses.js
@@ -2,9 +2,15 @@ import { Wrapper } from "./StudentClasses.styles";
 import { useEffect, useState } from "react";
 import StudentClassesHeader from "./StudentClasses.Header";
 import useGet from "../../custom-hooks/useGet";
+import { useNavigate, useParams } from "react-router-dom";
 
 const StudentClasses = () => {
   const [state, setState] = useState(undefined);
+  const navigator = useNavigate();
+  const params = useParams();
+  const nav = (classID) => {
+    navigator(`/${params.id}/classes/${classID}`);
+  };
   const schools = useGet(
     "http://localhost:8000/study/student/schools/",
     sessionStorage.getItem("token")
@@ -34,7 +40,7 @@ const StudentClasses = () => {
           classes.data.length &&
           classes.data.map((e) => {
             return (
-              <div key={e.id} className="flex-item">
+              <div key={e.id} className="flex-item" onClick={() => nav(e.id)}>
                 <div className="title">نام کلاس:</div>
                 <div className="school-name">{e.name}</div>
                 <div className="title">ارائه دهنده:</div>
