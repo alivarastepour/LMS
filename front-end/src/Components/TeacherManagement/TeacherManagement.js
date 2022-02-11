4e7fd832bfc3c6ddcfe266e35189cba6b7faa1c0
ali varaste pour <alivaraste@outlook.com>
2022-02-11 00:18:18 -0700

    TeacherManagement and relevant styles are added

---
+++ front-end/src/Components/TeacherManagement/TeacherManagement.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { Wrapper } from "./TeacherManagement.styles";
+import { Content } from "../SchoolInformation/SchoolInformation.styles";
+import TeacherManagementHeader from "./TeacherManagement.Header";
+import TeacherClasses from "../TeacherClasses/TeacherClasses";
+import SchoolList from "../SchoolList/SchoolList";
+
+const TeacherManagement = () => {
+  const defaultPage = useParams().info;
+  const [state, setState] = useState(defaultPage);
+  return (
+    <>
+      <Wrapper>
+        <TeacherManagementHeader state={state} setState={setState} />
+        <Content>
+          {state === "info" ? (
+            <TeacherClasses />
+          ) : (
+            <SchoolList content="teacher" />
+          )}
+        </Content>
+      </Wrapper>
+    </>
+  );
+};
+export default TeacherManagement;
