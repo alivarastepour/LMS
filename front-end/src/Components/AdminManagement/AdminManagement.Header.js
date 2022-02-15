ed90088fa0736097f1f7d12719e3e9634bfd3a66
ali varaste pour <alivaraste@outlook.com>
2022-02-15 09:41:36 -0700

    tab switch logic for admin panel is added

--- front-end/src/Components/AdminManagement/AdminManagement.Header.js
+++ front-end/src/Components/AdminManagement/AdminManagement.Header.js
@@ -1,12 +1,23 @@
-import React from "react";
 import { Wrapper } from "./AdminManagement.Header.styles";
 
-const AdminManagementHeader = () => {
+const AdminManagementHeader = ({ state, setState }) => {
   return (
     <>
       <Wrapper>
-        <button className="button  active">درخواست های ثبت مدرسه</button>
-        <button className="button margin inactive">مدارس ثبت شده</button>
+        <button
+          onClick={() => setState("req-sc")}
+          className={`button ${state === "req-sc" ? "active" : "inactive"}`}
+        >
+          درخواست های ثبت مدرسه
+        </button>
+        <button
+          onClick={() => setState("acc-sc")}
+          className={`button margin ${
+            state === "acc-sc" ? "active" : "inactive"
+          }`}
+        >
+          مدارس ثبت شده
+        </button>
       </Wrapper>
     </>
   );
--- front-end/src/Components/AdminManagement/AdminManagement.js
+++ front-end/src/Components/AdminManagement/AdminManagement.js
@@ -1,13 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import AdminManagementHeader from "./AdminManagement.Header";
 import { Wrapper } from "./AdminManagement.styles";
 import { Content } from "../SchoolInformation/SchoolInformation.styles";
 
 const AdminManagement = () => {
+  const [state, setState] = useState("req-sc");
   return (
     <>
       <Wrapper>
-        <AdminManagementHeader />
+        <AdminManagementHeader state={state} setState={setState} />
         <Content></Content>
       </Wrapper>
     </>
