2890b1e92144ece5d295bfd561f10d8541f869b8
ali varaste pour <alivaraste@outlook.com>
2022-02-15 09:18:36 -0700

    AdminManagement panel inits

---
+++ front-end/src/Components/AdminManagement/AdminManagement.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Wrapper } from "./AdminManagement.styles";
+
+const AdminManagement = () => {
+  return (
+    <>
+      <Wrapper></Wrapper>
+    </>
+  );
+};
+
+export default AdminManagement;
