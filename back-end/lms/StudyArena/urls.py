44c42c011050d43d2d8a53bfbd64ce61ac23eff5
Mahmood Choopani <choopani.m81@gmail.com>
2022-02-17 16:46:54 +0330

    add filter_option parameter.

--- back-end/lms/StudyArena/urls.py
+++ back-end/lms/StudyArena/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
 
 
     path('admin/schools/', AdminView.as_view(mode='schools')),
+    path('admin/schools/<str:filter_option>/', AdminView.as_view(mode='schools')),
     path('admin/meetings/', AdminView.as_view(mode='meetings')),
     path('admin/<str:school_id>/', AdminView.as_view(mode='schools'))
 ]
