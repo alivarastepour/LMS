ed22476df42bf7e0cebe91301b7498fa3c65110a
ali varaste pour <alivaraste@outlook.com>
2022-02-18 01:26:55 -0700

    AutoComplete component is removed(replaced with Select)

--- front-end/src/Components/AutoComplete/AutoComplete.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-  .css-8nbuqt-MuiAutocomplete-root
-    .MuiOutlinedInput-root.MuiInputBase-sizeSmall
-    .MuiAutocomplete-input {
-    font-family: "vazir", sans-serif;
-  }
-`;
