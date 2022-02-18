5306e022860751e19f8c52890dea63d027f681a9
ali varaste pour <alivaraste@outlook.com>
2022-02-18 00:46:41 -0700

    AllSchools body is added

---
+++ front-end/src/Components/AllSchools/AllSchoolsTable.js
@@ -0,0 +1,35 @@
+import { status } from "./Status";
+const AllSchoolsTable = ({ information }) => {
+  return (
+    <>
+      {information && information.length ? (
+        <table>
+          <tbody>
+            <tr>
+              <td className="header">ردیف</td>
+              <td className="header">شناسه</td>
+              <td className="header">نام مدرسه</td>
+              <td className="header">نام مدیر</td>
+              <td className="header">وضعیت</td>
+            </tr>
+            {information.map((a) => {
+              return (
+                <tr className="hover" key={a.id}>
+                  <td>{a.id}</td>
+                  <td>{a.school_id}</td>
+                  <td>{a.name}</td>
+                  <td>{a.manager}</td>
+                  <td>{status(a.status)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <div>dali</div>
+      )}
+    </>
+  );
+};
+
+export default AllSchoolsTable;
