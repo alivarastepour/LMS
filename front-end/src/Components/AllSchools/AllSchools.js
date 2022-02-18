1cbd2c489df45560e6d041fe3681abf3584e88e6
ali varaste pour <alivaraste@outlook.com>
2022-02-18 00:46:12 -0700

    AllSchools component and relevant styles are modified

---
+++ front-end/src/Components/AllSchools/AllSchools.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+import { Wrapper } from "./AllSchools.styles";
+
+import AllSchoolsHeader from "./AllSchools.Header";
+import AllSchoolsTable from "./AllSchoolsTable";
+
+import { searchHandler } from "./AllSchoolsHandlers";
+import useGet from "../../custom-hooks/useGet";
+
+const AllSchools = () => {
+  const [searchTerm, setSearchTerm] = useState("شناسه مدرسه");
+  const [searchValue, setSearchValue] = useState("");
+  const [information, setInformation] = useState([]);
+  const [staticInformation, setStaticInformation] = useState([]);
+  const [schoolFilter, setSchoolFilter] = useState("accepted");
+
+  const { data } = useGet(
+    `http://localhost:8000/study/admin/schools/${schoolFilter}`,
+    sessionStorage.getItem("token")
+  );
+
+  useEffect(() => {
+    setInformation(data);
+    setStaticInformation(data);
+  }, [data]);
+
+  return (
+    <>
+      <Wrapper>
+        <AllSchoolsHeader
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          searchTerm={searchTerm}
+          staticInformation={staticInformation}
+          setInformation={setInformation}
+          searchHandler={searchHandler}
+          setSearchTerm={setSearchTerm}
+          schoolFilter={schoolFilter}
+          setSchoolFilter={setSchoolFilter}
+        />
+        <AllSchoolsTable information={information} />
+      </Wrapper>
+    </>
+  );
+};
+
+export default AllSchools;
