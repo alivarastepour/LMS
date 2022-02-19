c4f6b44a5a7e35b25312f29afdcc84e6113ae1f4
ali varaste pour <alivaraste@outlook.com>
2022-02-19 08:24:21 -0700

    logout styles for header are added

--- front-end/src/Components/MainPage/Header.js
+++ front-end/src/Components/MainPage/Header.js
@@ -1,17 +1,35 @@
 import { Wrapper } from "./Header.styles";
 import logo from "../../Assets/photo_2022-01-21_13-58-45.jpg";
+import { authContext } from "../../App";
+import { useContext } from "react";
+import LogoutIcon from "@mui/icons-material/Logout";
 const f = "سیستم مدیریت آموزش ";
 const s = "(LMS) ";
 const t = "آتی گستر";
 const MainHeader = () => {
+  const { auth } = useContext(authContext);
   return (
     <>
       <Wrapper>
-        <div className="img-container">
-          <div>
-            <img src={logo} alt="ati gostar" className="logo" />
+        <div className="container">
+          <div className="img-text-container">
+            <div className="img-container">
+              <img src={logo} alt="ati gostar" className="logo" />
+            </div>
+            <div className="text">{f + s + t}</div>
           </div>
-          <div className="text">{f + s + t}</div>
+          <div>
+            {auth ? (
+              <div className="logout-container">
+                <div>
+                  <LogoutIcon />
+                </div>
+                <div>خروج</div>
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </Wrapper>
     </>
--- front-end/src/Components/MainPage/Header.styles.js
+++ front-end/src/Components/MainPage/Header.styles.js
@@ -8,35 +8,42 @@ export const Wrapper = styled.div`
   backdrop-filter: blur(31px) saturate(160%);
   -webkit-backdrop-filter: blur(30px) saturate(160%);
   background-color: rgba(255, 255, 255, 0.75);
-  .img-container {
+
+  .container {
     display: flex;
     flex-direction: row;
     align-items: center;
-    margin-left: 400px;
-    @media only screen and (max-width: 875px) {
-      & {
-        margin-left: 0;
-        justify-content: center;
-      }
-    }
-    @media only screen and (max-width: 490px) {
-      & {
-      }
+    justify-content: space-between;
+    padding-left: 10px;
+    direction: ltr;
+    .img-text-container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
     }
     .logo {
-      width: 50px;
-      height: 50px;
+      width: 40px;
+      height: 40px;
     }
     .text {
       font-family: "vazir", sans-serif;
-      font-size: 1.5rem;
       direction: rtl;
       margin-left: 10px;
-      color: #8b9ce8;
-      @media only screen and (max-width: 490px) {
-        & {
-          font-size: 0.8rem;
-        }
+    }
+    .logout-container {
+      margin-top: 5px;
+      display: flex;
+      flex-direction: row;
+      align-items: stretch;
+      font-family: "vazir", sans-serif;
+      cursor: pointer;
+      &:hover {
+        transition: 0.4s;
+        opacity: 0.5;
+      }
+      &:not(:hover) {
+        transition: 0.4s;
+        opacity: 1;
       }
     }
   }
