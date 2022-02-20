c860ae55c758d8b6e39f11c066970351edb13f3b
ali varaste pour <alivaraste@outlook.com>
2022-02-20 00:44:52 -0700

    RWD for Footer component is added

--- front-end/src/Components/Footer/Footer.styles.js
+++ front-end/src/Components/Footer/Footer.styles.js
@@ -3,24 +3,43 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   width: 40%;
   height: 70px;
+  direction: rtl;
   margin: auto;
+  padding-left: 10px;
+  padding-right: 10px;
   backdrop-filter: blur(31px) saturate(160%);
   -webkit-backdrop-filter: blur(30px) saturate(160%);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 10px 10px 0 0;
   display: flex;
   flex-wrap: wrap;
-  flex-direction: row-reverse;
-  justify-content: space-evenly;
-  align-content: center;
-  align-items: baseline;
+  flex-direction: row;
+  justify-content: space-between;
+  @media only screen and (max-width: 1340px) {
+    & {
+      width: 60%;
+    }
+  }
+  @media only screen and (max-width: 900px) {
+    & {
+      width: 100%;
+    }
+  }
+  @media only screen and (max-width: 350px) {
+    & {
+      flex-direction: column;
+      justify-content: center;
+      flex-wrap: wrap;
+      padding: 0;
+    }
+  }
 
   .p-des {
     font-family: "vazir", sans-serif;
   }
 
   .e-des {
-    font-family: sans-serif;
+    font-family: "Raleway", sans-serif;
   }
 
   @media only screen and (max-width: 800px) {
@@ -29,5 +48,26 @@ export const Wrapper = styled.div`
   @media only screen and (max-width: 620px) {
     width: 100%;
     font-size: 0.9rem;
+  }
+  .github {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    @media only screen and (max-width: 500px) {
+      .report {
+        display: none;
+      }
+    }
+  }
+  .copyright {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  .a {
+    color: black;
   }
 `;
