d8d81b9be0dc1926de804a3a9d832c8f9134abef
ali varaste pour <alivaraste@outlook.com>
2022-02-22 10:33:46 -0700

    GuestLogin is is splited to different components

--- front-end/src/Components/GuestLogin/GuestLogin.js
+++ front-end/src/Components/GuestLogin/GuestLogin.js
@@ -1,27 +1,29 @@
-import { useNavigate, useParams } from "react-router-dom";
-import Footer from "../Footer/Footer";
-import MainHeader from "../MainPage/Header";
-import { guestJoin } from "./GuestLoginHandlers";
-import { Wrapper, Content } from "./GuestLogin.styles";
 import { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { Alert, IconButton, Snackbar, Tooltip } from "@mui/material";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+
+import MainHeader from "../MainPage/Header";
+import Footer from "../Footer/Footer";
+import State from "./State";
+import Messages from "./Messages";
+import Actions from "./Actions";
+
+import { Wrapper } from "./GuestLogin.styles";
+
+import { guestJoin } from "./GuestLoginHandlers";
 
 const GuestLogin = () => {
   const classID = useParams().id;
   const navigatorr = useNavigate();
   const nav = () => navigatorr("/404");
+
   const [name, setName] = useState("");
   const [clazz, setClazz] = useState({});
   const [link, setLink] = useState("");
   const [message, setMessage] = useState("");
   const [success, setSuccess] = useState(true);
   const [open, setOpen] = useState(false);
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(link);
-  };
+
   const fetchData = useCallback(() => {
     axios
       .get(`http://localhost:8000/study/class/${classID}/name/`)
@@ -32,106 +34,44 @@ const GuestLogin = () => {
         }
       });
   }, [classID]);
-  const withInput = (Component) => {
-    if (!clazz.running) {
-      return (
-        <Tooltip
-          title={
-            <div style={{ fontFamily: "vazir" }}>کلاس هنوز آغاز نشده است</div>
-          }
-        >
-          {Component}
-        </Tooltip>
-      );
-    } else {
-      return Component;
-    }
-  };
+
   useEffect(() => {
     fetchData();
   }, [fetchData]);
+
   return (
     <>
       <MainHeader />
       <Wrapper>
-        <h1 className="headerBox">
-          ورود مهمان به {" " + clazz.class}, مدرسه {clazz.school}
-        </h1>
-        <div className="flex-container">
-          <div>
-            {withInput(
-              <input
-                type="text"
-                placeholder="نام خود را وارد کنید"
-                className="input"
-                autoFocus
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                disabled={!clazz.running}
-              />
-            )}
-          </div>
-          <div>
-            <button
-              className="button"
-              onClick={() =>
-                guestJoin(
-                  classID,
-                  name,
-                  setLink,
-                  setMessage,
-                  setSuccess,
-                  setOpen
-                )
-              }
-              disabled={!clazz.running}
-            >
-              <b>دریافت لینک ورود</b>
-            </button>
-          </div>
-        </div>
-        {link && (
-          <div className="link-container">
-            <div>
-              <Tooltip title="open">
-                <a href={link} target="_blank" rel="noreferrer">
-                  <IconButton>
-                    <OpenInNewIcon />
-                  </IconButton>
-                </a>
-              </Tooltip>
-            </div>
-            <div>
-              <Tooltip title="copy">
-                <IconButton
-                  onClick={() => {
-                    setMessage("لینک کلاس به کلیپ‌بورد الصاق شد");
-                    setSuccess(true);
-                    setOpen(true);
+        <State
+          name={name}
+          clazz={clazz}
+          setName={setName}
+          guestJoin={guestJoin}
+          classID={classID}
+          setLink={setLink}
+          setMessage={setMessage}
+          setSuccess={setSuccess}
+          setOpen={setOpen}
+        />
 
-                    copyToClipboard();
-                  }}
-                >
-                  <ContentCopyIcon />
-                </IconButton>
-              </Tooltip>
-            </div>
-          </div>
+        {link && (
+          <Actions
+            link={link}
+            setMessage={setMessage}
+            setSuccess={setSuccess}
+            setOpen={setOpen}
+          />
         )}
       </Wrapper>
       )
       <Footer />
-      <Content>
-        <Snackbar
-          open={open}
-          onClose={() => setOpen(false)}
-          autoHideDuration={3000}
-        >
-          <Alert severity={success ? "success" : "error"}>
-            <div className="message">{message}</div>
-          </Alert>
-        </Snackbar>
-      </Content>
+      <Messages
+        open={open}
+        setOpen={setOpen}
+        success={success}
+        message={message}
+      />
     </>
   );
 };
