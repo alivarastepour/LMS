0bfd15e8d8cf14b3fef22b4e14b9ee9881a5ba3c
Mahmood Choopani <choopani.m81@gmail.com>
2022-02-22 18:36:03 +0330

    add logout endpoint.

--- back-end/lms/AuthArena/urls.py
+++ back-end/lms/AuthArena/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import SignUp, Profile, CustomLogin
+from .views import SignUp, Profile, CustomLogin, Logout
 urlpatterns = [
     path('login/', CustomLogin.as_view()),
     path('signup/', SignUp.as_view()),
     path('whoami/', Profile.as_view()),
     path('whoami/<str:filter_option>/', Profile.as_view()),
+    path('logout/', Logout.as_view()),
 ]
