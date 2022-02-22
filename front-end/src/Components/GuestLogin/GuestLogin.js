2d9b96f5d673031f9488965436ff3c18a7b061c5
ali varaste pour <alivaraste@outlook.com>
2022-02-22 09:28:44 -0700

    class info fetch in guest login page

--- front-end/src/Components/GuestLogin/GuestLogin.js
+++ front-end/src/Components/GuestLogin/GuestLogin.js
@@ -1,32 +1,72 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import MainHeader from "../MainPage/Header";
+import { guestJoin } from "./GuestLoginHandlers";
 import { Wrapper } from "./GuestLogin.styles";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { Tooltip } from "@mui/material";
 
 const GuestLogin = () => {
-  const name = "کلاس اول";
+  const namex = "کلاس اول";
   const classID = useParams().id;
+  const navigator = useNavigate();
+  const nav = () => navigator("/404");
+  const [name, setName] = useState("");
+  const [clazz, setClazz] = useState({});
+  const fetchData = useCallback(() => {
+    axios
+      .get(`http://localhost:8000/study/class/${classID}/name/`)
+      .then((res) => setClazz(res.data))
+      .catch(function (e) {
+        if (e.response.status === 404) {
+          nav();
+        }
+      });
+  }, [classID, nav]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
     <>
       <MainHeader />
       <Wrapper>
-        <h1 className="headerBox">ورود مهمان به کلاس{" " + name}</h1>
+        <h1 className="headerBox">
+          ورود مهمان به {" " + clazz.class}, مدرسه {clazz.school}
+        </h1>
         <div className="flex-container">
           <div>
-            <input
-              type="text"
-              placeholder="نام خود را وارد کنید"
-              className="input"
-              autoFocus
-            />
+            <Tooltip
+              title={
+                <div style={{ fontFamily: "vazir" }}>
+                  کلاس هنوز آغاز نشده است
+                </div>
+              }
+            >
+              <input
+                type="text"
+                placeholder="نام خود را وارد کنید"
+                className="input"
+                autoFocus
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                disabled={!clazz.running}
+              />
+            </Tooltip>
           </div>
           <div>
-            <button className="button">
+            <button
+              className="button"
+              onClick={() => guestJoin(classID, name)}
+              disabled={!clazz.running}
+            >
               <b>دریافت لینک ورود</b>
             </button>
           </div>
         </div>
       </Wrapper>
+      )
       <Footer />
     </>
   );
